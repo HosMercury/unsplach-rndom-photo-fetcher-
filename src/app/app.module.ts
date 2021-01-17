e86fc3d8d2edf582d88812afb4ab50bb6b3e58f6
Hossam Maher <hossam@Hossams-MacBook-Pro.local>
2021-01-17 06:47:16 +0200

    bootstraping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { PhotoShowComponent } from './photo-show/photo-show.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoShowComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
